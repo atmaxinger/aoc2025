ec3c514821af48fdb5b5a82264406ef7f34c7e0c
Maximilian Irlinger <max@maxirlinger.at>
2025-12-04 19:26:10 +0100

    Day 4 - performance improvements

--- src/day4.2/main.rs
+++ src/day4.2/main.rs
@@ -9,6 +9,7 @@ fn main() {
 
 struct Grid {
     cells: Vec<char>,
+    rolls: Vec<(usize, usize)>,
     rows: usize,
     cols: usize,
 }
@@ -18,40 +19,46 @@ fn solve(reader: &mut dyn BufRead) -> i32 {
 
     let mut total_removed = 0;
     loop {
-        let movable_rolls = grid.get_movable_rolls();
-        if movable_rolls.is_empty() {
+        let removed = grid.remove();
+        if removed == 0 {
             break total_removed;
         }
-
-        total_removed += movable_rolls.len() as i32;
-
-        for (row, col) in movable_rolls {
-            grid.remove_roll(row, col);
-        }
+        total_removed += removed;
     }
 }
 
 fn parse_grid(reader: &mut dyn BufRead) -> Grid {
-    let lines: Vec<String> = reader
-        .lines()
-        .filter_map(|line| line.ok())
-        .collect();
+    let lines: Vec<String> = reader.lines().filter_map(|line| line.ok()).collect();
 
     let grid: Vec<char> = lines
         .iter()
         .flat_map(|line| line.chars().collect::<Vec<char>>())
         .collect();
 
+    let rows = lines.len();
+    let cols = lines[0].len();
+    let mut rolls = Vec::new();
+
+    for row in 0..rows {
+        for col in 0..cols {
+            let idx = row * cols + col;
+            if grid[idx] == '@' {
+                rolls.push((row, col));
+            }
+        }
+    }
+
     Grid {
         cells: grid,
-        rows: lines.len(),
-        cols: lines[0].len(),
+        rolls,
+        rows,
+        cols,
     }
 }
 
 impl Grid {
     fn get_neighbors(&self, row: usize, col: usize) -> Option<i32> {
-        let directions: [(isize,isize);8] = [
+        let directions: [(isize, isize); 8] = [
             (-1, -1),
             (0, -1),
             (1, -1),
@@ -62,46 +69,50 @@ impl Grid {
             (1, 1),
         ];
 
-        match self.cells.get(row * self.cols + col) {
-            None => None,
-            Some('.') => None,
-            _ => {
-                let mut neighbors = 0;
-                for (dc, dr) in directions.iter() {
-                    let new_row = row as isize + dr;
-                    let new_col = col as isize + dc;
-                    if new_row >= 0 && new_row < self.rows as isize
-                        && new_col >= 0 && new_col < self.cols as isize
-                        && self.cells[new_row as usize * self.cols + new_col as usize] == '@' {
-                        neighbors += 1;
-                    }
-                }
-
-                Some(neighbors)
-            },
+        if row >= self.rows || col >= self.cols {
+            return None;
         }
-    }
 
-    fn get_movable_rolls(&self) -> Vec<(usize, usize)> {
-        let mut movable_rolls = Vec::new();
+        if self.cells[row * self.cols + col] == '.' {
+            return None;
+        }
 
-        for row in 0..self.rows {
-            for col in 0..self.cols {
-                if let Some(neighbors) = self.get_neighbors(row, col) {
-                    if neighbors < 4 {
-                        movable_rolls.push((row, col));
-                    }
-                }
+        let mut neighbors = 0;
+        for (dc, dr) in directions.iter() {
+            let new_row = row as isize + dr;
+            let new_col = col as isize + dc;
+            if new_row >= 0
+                && new_row < self.rows as isize
+                && new_col >= 0
+                && new_col < self.cols as isize
+                && self.cells[new_row as usize * self.cols + new_col as usize] == '@'
+            {
+                neighbors += 1;
             }
         }
 
-        movable_rolls
+        Some(neighbors)
     }
 
-    fn remove_roll(&mut self, row: usize, col: usize) {
-        if let Some(cell) = self.cells.get_mut(row * self.cols + col) {
-            *cell = '.';
+    fn remove(&mut self) -> i32 {
+        let mut removed = 0;
+
+        let mut next_rolls = Vec::new();
+        for (row, col) in self.rolls.iter() {
+            if let Some(neighbors) = self.get_neighbors(*row, *col) {
+                if neighbors < 4 {
+                    if let Some(cell) = self.cells.get_mut(row * self.cols + col) {
+                        *cell = '.';
+                        removed += 1;
+                    }
+                } else {
+                    next_rolls.push((*row, *col));
+                }
+            }
         }
+        self.rolls = next_rolls;
+
+        removed
     }
 }
 
@@ -115,16 +126,6 @@ mod tests {
         let mut reader = std::io::Cursor::new(input);
         let result = super::solve(&mut reader);
         assert_eq!(result, 43);
-    }
-
-    #[test]
-    fn test_get_movable_rolls() {
-        let input = "..@@.@@@@.\n@@@.@.@.@@\n@@@@@.@.@@\n@.@@@@..@.\n@@.@@@@.@@\n.@@@@@@@.@\n.@.@.@.@@@\n@.@@@.@@@@\n.@@@@@@@@.\n@.@.@@@.@.";
-
-        let grid = super::parse_grid(&mut std::io::Cursor::new(input));
-        let movable_rolls = grid.get_movable_rolls();
-
-        assert_eq!(movable_rolls.len(), 13);
     }
 
     #[test]
@@ -142,7 +143,10 @@ mod tests {
         let grid = super::parse_grid(&mut reader);
         assert_eq!(grid.rows, 3);
         assert_eq!(grid.cols, 4);
-        assert_eq!(grid.cells, vec!['.', '.', '@', '.', '@', '@', '.', '.', '.', '@', '.', '@']);
+        assert_eq!(
+            grid.cells,
+            vec!['.', '.', '@', '.', '@', '@', '.', '.', '.', '@', '.', '@']
+        );
     }
 
     #[rstest]
